67c86fc2b8c946ce19796deb57ab71c8fb4f71ba
Alex Mitchell <alex@alexthemitchell.com>
2016-11-10 13:12:55 -0600

    Add code from Monday

--- Directory/ViewController.m
+++ Directory/ViewController.m
@@ -1,29 +1,47 @@
-//
-//  ViewController.m
-//  Directory
-//
-//  Created by Alex Mitchell on 11/7/16.
-//  Copyright © 2016 Grinnell AppDev. All rights reserved.
-//
-
 #import "ViewController.h"
+#import "Person.h"
 
 @interface ViewController ()
 
 @end
 
-@implementation ViewController
+@implementation ViewController {
+  NSArray<Person *> *people;
+}
 
 - (void)viewDidLoad {
   [super viewDidLoad];
-  // Do any additional setup after loading the view, typically from a nib.
+  Person *alex = [Person new];
+  alex.name = @"Alex";
+  alex.title = @"instructor";
+  
+  Person *bob = [Person new];
+  bob.name = @"bob";
+  bob.title = @"example";
+  
+  people = @[alex, bob];
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+  return people.count;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+  UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+  Person *person = people[indexPath.row];
+  cell.textLabel.text = person.name;
+  cell.detailTextLabel.text = person.title;
+  return cell;
 }
 
 
-- (void)didReceiveMemoryWarning {
-  [super didReceiveMemoryWarning];
-  // Dispose of any resources that can be recreated.
-}
+
+
+
+
+
+
+
 
 
 @end
---
+++ Person.h
@@ -0,0 +1,7 @@
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+@property (strong, nonatomic) NSString *name;
+@property (strong, nonatomic) NSString *title;
+
+@end

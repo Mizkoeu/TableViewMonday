c68e7228edf3e901b3235b59562bed78450ae281
Alex Mitchell <alex@alexthemitchell.com>
2016-11-16 21:05:40 -0600

    Add code to fetch from JSON

--- Directory/ViewController.m
+++ Directory/ViewController.m
@@ -1,25 +1,45 @@
 #import "ViewController.h"
 #import "Person.h"
 
+static NSString *dataURL =
+  @"https://appdev.grinnell.edu/members.json";
+
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
 @implementation ViewController {
-  NSArray<Person *> *people;
+  NSMutableArray<Person *> *people;
 }
 
 - (void)viewDidLoad {
   [super viewDidLoad];
-  Person *alex = [Person new];
-  alex.name = @"Alex";
-  alex.title = @"instructor";
+  [self refreshContent];
   
-  Person *bob = [Person new];
-  bob.name = @"bob";
-  bob.title = @"example";
-  
-  people = @[alex, bob];
+  people = [NSMutableArray new];
+}
+
+- (void)refreshContent {
+  NSURL *properDataURL = [NSURL URLWithString:dataURL];
+  NSURLSessionDataTask *task =
+    [[NSURLSession sharedSession] dataTaskWithURL:properDataURL completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+      NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+      
+      NSArray<NSString *> *names = [dict allKeys];
+      
+      for (NSString *name in names) {
+        NSDictionary *personDict = dict[name];
+        NSString *title = personDict[@"title"];
+        Person *person = [Person new];
+        person.name = name;
+        person.title = title;
+        
+        [people addObject:person];
+      }
+      [self.tableView reloadData];
+    }];
+  [task resume];
 }
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
